38fd6b01b09c5f72b11ca4c79522bd66ff4efee0
Exusai <a.a.r.samuel99@gmail.com>
2021-10-17 22:01:02 -0500

    added images

--- src/App.svelte
+++ src/App.svelte
@@ -131,7 +131,7 @@
 				<p class="lead">Como consecuencia de la explotación de los mantos acuiferos, la Ciudad de México se hunde constantemente. En algunas partes, la ciudad se hunde hasta 0.5 metros anualmente y gracias a la plancha de asfalto que cubre la ciudad no se puede reintegrar el agua de la lluvias a los mantos acuiferos.</p>
 			</div>
 			<div class="col-sm">
-				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/hundimiento.png" alt="Hundimiento en la ciudad">
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/hundimiento.png" alt="Hundimiento en la ciudad" width="800">
 			</div>
 		</div>
 	</section>
@@ -139,7 +139,7 @@
 	<section>
 		<div class="row align-items-center">
 			<div class="col-sm">
-				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/hundimiento2.png" alt="Hundimiento ejemplo 2">
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/hundimiento2.png" alt="Hundimiento ejemplo 2" width="800">
 			</div>
 			<div class="col-sm">
 				<!-- <h3><span id="w1">Hundimiento</span></h3> -->
@@ -156,7 +156,7 @@
 				<p class="lead">Debido a que la Ciudad no puede seguir extrayendo agua de su subsuelo esta importa el 40% de su suminstro de agua a través de un sistema de canales y tuberías.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/Canales.png" alt="Canales y tuberías de agua" width="800">
 			</div>
 		</div>
 	</section>
@@ -164,7 +164,7 @@
 	<section>
 		<div class="row align-items-center">
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/waterLoss.jpg" alt="Pérdidas de agua" width="800">
 			</div>
 			<div class="col-sm">
 				<p class="lead">Este sistema de canales es tan ineficiente que de cada litro de agua bombeado por el mismo, se estima que más de un tercio se pierde en el transporte. Sin mencionar que el hundimiento de la Ciudad causa que las tuberías se rompan constantemente.</p>
@@ -179,7 +179,7 @@
 				<p class="lead">Al tener que importar tanta agua, la Ciudad de México utiliza tanta energía como la Ciudad de Puebla SOLAMENTE para bombear agua. Esto es debido a que el agua debe ser bombeada desde zonas menos elevadas que la ciudad.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/cutzamala-2.jpg" alt="Sistema de agua" width="800">
 			</div>
 		</div>
 	</section>
@@ -187,7 +187,7 @@
 	<section>
 		<div class="row align-items-center">
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/water%20shortages.jpg" alt="Water shortages" width="800">
 			</div>
 			<div class="col-sm">
 				<p class="lead">Por esta razón la ciudad sufre de tanta escasez de agua como si se encontrase en un desierto, a pesar de que recive incluso más lluvia que Londres.</p>
@@ -202,7 +202,7 @@
 				<p class="lead">La solución que se propone consta de diversos sistemas distribuidos y aplicados por toda la ciudad. Estos estarían basados en el Computo en la Nube, IoT, Machine Learning y Big Data.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="" alt="Smart water" width="800">
 			</div>
 		</div>
 	</section>
@@ -214,7 +214,7 @@
 				<p class="lead">Se proponen diversos sitemas inteligentes de recolección de agua en las calles y edificios, los cules sean capaces de reingresar agua a los sistemas subterraneos o de abastecer a su respectivo edificio dependiedo de lo que la "nube" determine que sea más necesario.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/rain%20water%20harvesting.webp" alt="Water harvesting" width="800">
 			</div>
 		</div>
 	</section>
@@ -226,7 +226,7 @@
 				<p class="lead">Se sabe que una porción importante del agua desperdiciada es causada por infraestructura deficiente en casas y edificios. Para abordar este problema se rediseñaría esta misma infraestructura pero con IoT y conección a la nube, de modo que las fugas puedan ser detectadas y reparadas lo más pronto posible.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/water%20iot.jpg" alt="Water IoT" width="800">
 			</div>
 		</div>
 	</section>
@@ -238,13 +238,15 @@
 				<p class="lead">De manera similar a las redes de distribución de energía de las ciudades inteligentes, la propuesta es tener una red que distribuya el agua en función de donde se necesita más o donde puede ser almacenada si es que no se necesita.</p>
 			</div>
 			<div class="col-sm">
-				img
+				<img src="https://github.com/Exusai/a-qua/raw/master/imgs/smat%20water.jpeg" alt="Smart grid" width="800">
 			</div>
 		</div>
 	</section>
 
 	<section>
 		<h1><span id="w1">Acerca </span><span id="w2">de esta </span><span id="w3">página</span></h1>
+		<p class="lead">Algunas imágenes fueron tomadas de un video de RealLifeLore, muy recomendado.</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/eAL1kYjsVHE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</section>
 
 	<!-- <section>
